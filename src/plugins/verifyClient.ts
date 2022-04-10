d4b4540add38812be51c581f90fb7563b15b3ce7
olcay <olcayusta02@gmail.com>
2022-04-10 22:49:48 +0300

    Remove unused plugin

--- src/plugins/verifyClient.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
-
-export default async function (app: FastifyInstance) {
-    app.addHook('preHandler', (req: FastifyRequest, reply: FastifyReply, done) => {
-        console.log('Oki toki')
-    })
-}
