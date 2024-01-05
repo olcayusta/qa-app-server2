a5a7b7211d0a08ddfffea8ff92b7dcfadbe78079
Olcay Usta <olcayusta02@gmail.com>
2024-01-06 01:14:18 +0300

    added

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -4,6 +4,6 @@ import fastifyCors, { FastifyCorsOptions } from '@fastify/cors'
 
 export default fp<FastifyCorsOptions>(async (fastify: FastifyInstance) => {
   fastify.register(fastifyCors, {
-    origin: ['http://localhost:4200', 'http://localhost:3000', 'http://192.168.1.7:3000', 'http://192.168.1.7:4200', 'https://batman-app.vercel.app']
+    origin: ['http://localhost:4201', 'http://localhost:4200', 'http://localhost:3000', 'http://192.168.1.7:3000', 'http://192.168.1.7:4200', 'https://batman-app.vercel.app']
   })
 })
